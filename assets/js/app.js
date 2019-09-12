74ce5b42dbbd83fced1d64e55d8f7e06bfdc836a
Tyler Winters <maxdavin1984@gmail.com>
2019-09-12 15:49:22 -0700

    igdb api working in console, not to full expectation

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,6 @@ function renderButtons() {
        var gifButton = $('<button>');
         gifButton.attr("data-twitch", topics[i]);
         gifButton.text(topics[i]);
-        // gifButton.addClass("gifGetter");
-        // gifButton.addClass('btn btn-primary');
-        // gifButton.addClass('button-color');
         $('#buttons-view').append(gifButton);
     }
 }
@@ -18,17 +15,6 @@ renderButtons();
 
 
 $(document).on('click', function(){
-
-    // var settings = {
-    //     "async": true,
-    //     "crossDomain": true,
-    //     "url": "https://chicken-coop.p.rapidapi.com/games/Tomba?platform=Playstation%201",
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-host": "chicken-coop.p.rapidapi.com",
-    //         "x-rapidapi-key": "87bb7a25d8msh7bd72b884b57b59p1ac2eajsnb6cb082269e7"
-    //     }
-    // }
     
     $.ajax({
     url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
@@ -36,7 +22,7 @@ $(document).on('click', function(){
     headers: {
         'user-key': '6d738ccad4eec052f368a4959500b715',
         },
-        data: 'search "Yakuza"; fields name, involved_companies;'
+        data: 'search "dead cells"; fields summary, collection, name, age_ratings, cover, aggregated_rating, total_rating, videos;'
     })
       .then(response => {
           console.log(response);
@@ -44,27 +30,8 @@ $(document).on('click', function(){
       .catch(err => {
           console.error(err);
       });
-
-
-    // }).then(function(response) {
-    //     console.log(response)
     
-    
-    
-    
-    // $.ajax(settings).done(function (response) {
-    //     console.log(response);
-    // });
-
-
     
 })
 
-    var queryURL = "https://api-docs.igdb.com/#about";
-
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response) {
-        console.log(response)
-    })
+ 
