902e1e84e7d9f65ea4767941a179789389a9f09a
Alex F <farrisat1@gmail.com>
2019-09-16 11:05:38 -0700

    Single ajax call

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,7 @@
 //IGDB calls 
 var queryURLGames = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games";
 var queryURLGenres = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/genres";
-var queryURLCovers = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers";
 var queryURLFeeds = "";
-var queryURLArtworks = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/artworks";
 var queryURLPlatforms = "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/platforms";
 // unixOneYear is the seconds for 365 days
 var unixOneYear= 31536000
@@ -11,7 +9,7 @@ var unixOneYear= 31536000
 var unixCurrentTime =  Math.floor((Date.now()) / 1000);
 //
 var queryTime = unixCurrentTime - unixOneYear
-
+//data: "fields cover.url, genres.name, name, first_release_date, storyline,summary, platforms.name, total_rating, videos; where total_rating > 95; "+"; sort total_rating desc; limit 10; where cover != null;"
 //calling IGDB for the game name
 $.ajax({
     url: queryURLGames,
@@ -19,7 +17,7 @@ $.ajax({
     headers: {
       "user-key": "d6a65b1a245c294e5c6aad8ed4c649c1",
     },
-    data: "fields cover, genres, name, first_release_date, storyline,summary, platforms, artworks, total_rating, videos; where total_rating>95; where first_release_date.date>"+queryTime +";sort total_rating desc; limit 10; where cover!=null;"
+    data: "fields cover.url, genres.name, name, first_release_date, storyline,summary, platforms.name, total_rating, videos; sort total_rating desc; limit 10; where cover != null & summary != null & genres != null & platforms != null & storyline != null & first_release_date != null &first_release_date>"+queryTime +";"
   })
   .then(function (response) {
     console.log(response)
@@ -31,52 +29,21 @@ $.ajax({
 
 //function to get top ten images and append images and name to page. Also to call the general game information for the middle section.
 function getCover(data) {
-  $.ajax({
-      url: queryURLCovers,
-      method: "POST",
-      headers: {
-        "user-key": "d6a65b1a245c294e5c6aad8ed4c649c1",
-      },
-      data: "fields game, url; where game = " + data.id + ";"
-    })
-    .then(function (outer_response) {
 
       // Platform information
-      $.ajax({
-          url: queryURLPlatforms,
-          method: "POST",
-          headers: {
-            "user-key": "d6a65b1a245c294e5c6aad8ed4c649c1",
-          },
-          data: "fields name, generation; where id =" + data.platforms[0] + ";"
-        })
-        .then(function(response) {
-          $.ajax({
-            url: queryURLGenres,
-            method: "POST",
-            headers: {
-              "user-key": "d6a65b1a245c294e5c6aad8ed4c649c1",
-            },
-            data: "fields name; where id =" + data.genres[0] + ";"
-          })
-          .then(function (genreresponse) {
-          // Beginning call GameInfo and append 
-            var image = $("<img>")
-            image.attr("src", "http:" + outer_response[0].url);
-            $(".containerOneGame").append(image);
-            $(".containerOneGame").append(data.name);
-            pullGameInfo(genreresponse[0].name, response[0].name, image, data.total_rating, data.storyline, data.summary, data.first_release_date)
-          })
-        })
-    })
+
+  // Beginning call GameInfo and append 
+    var image = $("<img>")
+    if (data.cover === undefined) {
+      console.log("UNDEF: " + data.name)
+    }
+    image.attr("src", "http:" + data.cover.url);
+    $(".containerOneGame").append(image);
+    $(".containerOneGame").append(data.name);
+    console.log(data);
+    pullGameInfo(data.genres[0].name, data.platforms[0].name, image, data.total_rating, data.storyline, data.summary, data.first_release_date)
+    //pullGameInfo(data.genres[0].name, data.platforms[0].name, image, data.total_rating, data.storyline, data.summary, data.first_release_date)
+    
+   
 }
 
-//calling IGDB for the alt artwork 
-// $.ajax({
-//   url: queryURLArtworks,
-//   method: "POST",
-//   headers: {
-//     "user-key": "d6a65b1a245c294e5c6aad8ed4c649c1",
-//   },
-//   data: 
-//   "games, height, width, image_id, url; where game = " + data.id +";"
