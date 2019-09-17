2fe0c1035ee4bc3fb0dd4aadcacd59cf662d53e5
Tyler Winters <maxdavin1984@gmail.com>
2019-09-17 11:54:48 -0700

    updated article api JS to properly pull wanted articles to display on index

--- assets/js/gamespot.js
+++ assets/js/gamespot.js
@@ -1,31 +1,26 @@
 
-var gameSpotURL = "https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/games/?api_key=ae89892595c24a1771b20007d69c34c8e57bc96a&format=json&limit=10&filter=publish_date:2018-01-01|2019-06-01,articles_api_url"
+var gameSpotURL = "https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/articles/?api_key=ae89892595c24a1771b20007d69c34c8e57bc96a&format=json&limit=10&filter=publish_date:2018-01-01|2019-06-01,articles_api_url"
 
 $.ajax({
     url: gameSpotURL,
     method: 'GET'
-    // data: 
 }).then(function(response) {
     console.log(response);
     var results = response.results;
 
         for (let i = 0; i < results.length; i++) {
-            // var articleDiv = $('<div>')
 
-            var demo = response.results[i].name;
-            $('#article').append(demo)
-
-
-            // var demo = results[i].deck;
-
-            // var p = $('<p>').text(demo);
-
-            // articleDiv.append(p)
-
-            // $('#article').prepend(articleDiv);  
-            
+            var articleTitle = response.results[i].title;
+            var urlLink = response.results[i].site_detail_url;
+            var infoDiv = $("<div>")
+            infoDiv.addClass("")
+            var str = articleTitle
+            var urlClick = str.link(urlLink)
+            infoDiv.append(urlClick);
+            $('#article').append(infoDiv);   
         }
-
-    
 })
+
+
+
 
